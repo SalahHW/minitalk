a0f855799f425dc8a92db23cae366e4194b4aaeb
SalahHW <salah.bouheniche@gmail.com>
2023-06-06 02:03:16 +0200

    add function that handle complete string to send

--- include/minitalk.h
+++ include/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 04:20:01 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/06/06 01:29:04 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/06/06 02:00:55 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include <stdlib.h>
 
 void	sig_handler(int signal);
+void	send_message(int server_pid, char *message);
 void	send_bits_from_char(int pid, unsigned char c);
 
 #endif
--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 04:16:45 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/06/06 01:29:24 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/06/06 02:00:33 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	main(int argc, char **argv)
 {
 	int	server_pid;
+	char *argv_ptr;
 
 	if (argc != 3)
 	{
@@ -22,8 +23,19 @@ int	main(int argc, char **argv)
 		ft_putstr_fd("./client \"server PID\" \"message\"\n", 2);
 		exit(1);
 	}
+	argv_ptr = argv[2];
 	server_pid = ft_atoi(argv[1]);
-	send_bits_from_char(server_pid, *argv[2]);
+	send_message(server_pid, argv_ptr);
+}
+
+void	send_message(int server_pid, char *message)
+{
+	while (*message)
+	{
+		send_bits_from_char(server_pid, *message);
+		message++;
+	}
+	send_bits_from_char(server_pid, '\0');
 }
 
 void	send_bits_from_char(int pid, unsigned char c)
