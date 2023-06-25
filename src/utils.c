ab28b22141354830161a7d45b3d121c2763a4ddf
SalahHW <salah.bouheniche@gmail.com>
2023-06-26 00:49:20 +0200

    fix leak from *str

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 07:46:44 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/06/13 07:47:21 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/06/25 23:13:11 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ char	*join_char_to_string(char *str, unsigned char c)
 {
 	char	*new_string;
 	char	*new_string_ptr;
+	char	*str_ptr;
 
 	if (!str && !c)
 		return (NULL);
@@ -23,8 +24,11 @@ char	*join_char_to_string(char *str, unsigned char c)
 	if (!new_string)
 		return (NULL);
 	new_string_ptr = new_string;
-	while (*str)
-		*new_string_ptr++ = *str++;
+	str_ptr = str;
+	while (*str_ptr)
+		*new_string_ptr++ = *str_ptr++;
 	*new_string_ptr = c;
+	free(str);
+	str = NULL;
 	return (new_string);
 }
