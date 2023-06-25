eb798636df0b2c6787dc42ff3c591d261de0da5d
SalahHW <salah.bouheniche@gmail.com>
2023-06-26 00:51:03 +0200

    use signal instead of sigaction

--- src/client.c
+++ src/client.c
@@ -6,13 +6,13 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 04:16:45 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/06/24 19:51:53 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/06/25 19:10:45 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
 
-int			g_sig_pending;
+int			g_sig_status;
 static void	send_bits_from_char(int pid, unsigned char c)
 {
 	int	bit_count;
@@ -20,12 +20,12 @@ static void	send_bits_from_char(int pid, unsigned char c)
 	bit_count = 7;
 	while (bit_count >= 0)
 	{
-		g_sig_pending = 1;
+		g_sig_status = 1;
 		if (1 & c >> bit_count)
 			kill(pid, SIGUSR2);
 		else
 			kill(pid, SIGUSR1);
-		while (g_sig_pending)
+		while (g_sig_status)
 			;
 		bit_count--;
 	}
@@ -36,7 +36,7 @@ static void	client_sig_handler(int signum)
 	if (signum == SIGUSR1)
 		ft_printf("Message received\n");
 	else
-		g_sig_pending = 0;
+		g_sig_status = 0;
 }
 
 static void	send_message(int server_pid, char *message)
@@ -51,18 +51,14 @@ static void	send_message(int server_pid, char *message)
 
 int	main(int argc, char **argv)
 {
-	struct sigaction usr1;
-	struct sigaction usr2;
 	int		server_pid;
 	char	*argv_ptr;
 
-	g_sig_pending = 0;
-	usr1.sa_handler = &client_sig_handler;
-	usr2.sa_handler = &client_sig_handler;
 	parse(argc, argv);
 	server_pid = ft_atoi(argv[1]);
 	argv_ptr = argv[2];
-	sigaction(SIGUSR1, &usr1, NULL);
-	sigaction(SIGUSR2, &usr2, NULL);
+	signal(SIGUSR1, client_sig_handler);
+	signal(SIGUSR2, client_sig_handler);
+	g_sig_status = 0;
 	send_message(server_pid, argv_ptr);
 }
